08c38e9426041e0d9b05785d6f80475a6bc85f16
Bailey Townsend <baileytownsend2323@gmail.com>
2021-07-11 15:29:20 -0500

    first bit

--- src/main/java/com/clanmate_export/ClanMateExportConfig.java
+++ src/main/java/com/clanmate_export/ClanMateExportConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.clanmate_export;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
-public interface ExampleConfig extends Config
+@ConfigGroup("clanmate_export")
+public interface ClanMateExportConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/test/java/com/clanmate_export/ClanMateExportTest.java
+++ src/test/java/com/clanmate_export/ClanMateExportTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.clanmate_export;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ClanMateExportTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ClanMateExportPlugin.class);
 		RuneLite.main(args);
 	}
 }
